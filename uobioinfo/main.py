d00e3819b60756c07d57db691c5149950d512305
Alexander OSTROVSKY <alexanderostrovsky@Cave-Johnson.local>
2018-10-11 13:21:41 -0700

    asdf

--- uobioinfo/main.py
+++ uobioinfo/main.py
@@ -3,7 +3,9 @@ import argparse
 
 def say_something(phrase): # ... I'm giving up on you. 
     """Change this however you'd like!"""
+    print("testphrase1")
     print(phrase)
+    print("testphrase2")
 
 
 # --- Don't change anything below this line -------
